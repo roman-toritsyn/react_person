3e5fb6e5ac2ed7c0f8360349e3358f489c3e0d0c
Roman Toritsyn <romantoritsyn@gmail.com>
2021-10-08 23:59:45 +0300

    Updates

---
+++ precache-manifest.c9ed2e715d0e246340f52f4a9436fb4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbfdd5582a8ca549db56cbbb0f118ad3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2e61435b98f2d74cce9a",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e61435b98f2d74cce9a",
+    "url": "./static/js/main.64fbdae5.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
